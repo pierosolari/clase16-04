c74bfbf08ae8ee453f429f0257425304441f8f7c
pierosolari <166528051+pierosolari@users.noreply.github.com>
2024-05-07 06:43:52 +0000

    avance2

--- src/partea.js
+++ src/partea.js
@@ -4,6 +4,8 @@ const Partea = ({ partea }) => {
     <ul class="nav">
  <li class="nav-item">
  <img src="https://img.freepik.com/vector-premium/icono-perfil-usuario-estilo-plano-ilustracion-vector-avatar-miembro-sobre-fondo-aislado-concepto-negocio-signo-permiso-humano_157943-15752.jpg" class="img-thumbnail" alt="..."></img>
+          
+          
           <h1>Iniciar Sesion</h1>
           <br></br>
           <h6> Inicia sesion para continuar </h6> 
